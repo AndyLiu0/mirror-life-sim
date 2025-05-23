a8229ba47947b80ed2a9a88402c28228895b3018
AndyLiu0 <aliu3@student.smusd.us>
2025-05-23 09:49:17 -0700

    debug menu continuous running

--- project/debug.gd
+++ project/debug.gd
@@ -6,37 +6,53 @@ var last_node: Label
 var out_node: Label
 var history = []
 var history_idx = 0
+var cont_button: CheckButton
+var expression = Expression.new()
+
 
 func _ready():
 	sim_node = get_node("../../Simulation")
 	in_node = get_node("In")
-	last_node = get_node("Last")
+	last_node = get_node("Middle/Last")
 	out_node = get_node("Out")
+	cont_button = get_node("Middle/PanelContainer/VBoxContainer/CheckButton")
 
 func _input(event):
 	if event.is_action_pressed("ui_debug"):
 		visible = !visible
 		if visible:
 			in_node.grab_focus()
+		get_viewport().set_input_as_handled()
 	
 	if visible:		
-		if in_node.has_focus():
-			if event is InputEventKey and event.is_pressed():
-				if OS.get_keycode_string(event.keycode) == "Enter":
-					get_viewport().set_input_as_handled()
-	
-		if event.is_action_pressed("ui_accept"):
-			var expression = Expression.new()
-			last_node.text = in_node.text
-			expression.parse(in_node.text)
-			print(expression.execute([], sim_node))
-			out_node.text = str(expression.execute([], sim_node))
-			history.append(in_node.text)
-			history_idx = len(history)
-			in_node.text = ""
+		if event.is_action_pressed("ui_cancel"):
+			visible = false
+			get_viewport().set_input_as_handled()
+			
+		if event.is_action_pressed("ui_accept") and in_node.has_focus():
+			parse()
+			get_viewport().set_input_as_handled()
 			
 		if event.is_action_pressed("ui_up"):
 			if history_idx > 0:
 				history_idx -= 1
 			in_node.text = history[history_idx]
-			
+		
+		if event.is_action_pressed("ui_debug_toggle_cont"):
+			cont_button.button_pressed = !cont_button.button_pressed
+
+func _process(delta):
+	if cont_button.button_pressed:
+		run()
+
+func run():
+	out_node.text = str(expression.execute([], sim_node))
+
+func parse():
+	if in_node.text != "":
+		last_node.text = in_node.text
+		expression.parse(last_node.text)
+		run()
+		history.append(in_node.text)
+		history_idx = len(history)
+		in_node.text = ""	
